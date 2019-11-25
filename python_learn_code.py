b6327a0a9747baa69ba4604b3ea725ed64af638d
nnjustin44 <nnjustin44@gmail.com>
2019-11-25 12:16:11 -0500

    Working out bugs from new user to an old user

--- python_learn_code.py
+++ python_learn_code.py
@@ -10,8 +10,8 @@ password=raw_input('Password:')
 database_sqlite.get_data()
 database_sqlite.create_user(user,password)
 
-if database_sqlite.get_data():
-    print("New Username Successful")
+if database_sqlite.get_data()==database_sqlite.tables():
+    print("Welcome Back User")
 else:
     new_user = raw_input('Create New Account Give Us A User Name: ')
     new_password = raw_input('Now Lets Get You A Password:')
